90b9a4f25bd51c0d9b87cc9e646d3cfa25ee7311
SovietTuna <99210398+SovietTuna@users.noreply.github.com>
2022-02-11 12:18:36 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,76 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
+    game.over(true, effects.confetti)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.over(false, effects.slash)
+})
+let araba: Sprite = null
+let Ordekk: Sprite = null
+tiles.setCurrentTilemap(tilemap`level1`)
+let oyuncu = sprites.create(img`
+    . . 4 4 4 . . . . 4 4 4 . . . . 
+    . 4 5 5 5 e . . e 5 5 5 4 . . . 
+    4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+    4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+    e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+    . e e 5 5 5 5 5 5 5 5 e e . . . 
+    . . e 5 f 5 5 5 5 f 5 e . . . . 
+    . . f 5 5 5 4 4 5 5 5 f . . f f 
+    . . f 4 5 5 f f 5 5 6 f . f 5 f 
+    . . . f 6 6 6 6 6 6 4 4 f 5 5 f 
+    . . . f 4 5 5 5 5 5 5 4 4 5 f . 
+    . . . f 5 5 5 5 5 4 5 5 f f . . 
+    . . . f 5 f f f 5 f f 5 f . . . 
+    . . . f f . . f f . . f f . . . 
+    `, SpriteKind.Player)
+oyuncu.setPosition(80, 10)
+controller.moveSprite(oyuncu)
+scene.cameraFollowSprite(oyuncu)
+game.onUpdateInterval(500, function () {
+    Ordekk = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . b 5 5 b . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . b b b b b 5 5 5 5 5 5 5 b . . 
+        . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+        . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+        . . b d 5 5 b 1 f f 5 4 4 c . . 
+        b b d b 5 5 5 d f b 4 4 4 4 b . 
+        b d d c d 5 5 b 5 4 4 4 4 4 4 b 
+        c d d d c c b 5 5 5 5 5 5 5 b . 
+        c b d d d d d 5 5 5 5 5 5 5 b . 
+        . c d d d d d d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `, SpriteKind.Enemy)
+    Ordekk.setVelocity(50, 0)
+    tiles.placeOnRandomTile(Ordekk, sprites.castle.tilePath5)
+    Ordekk.x = 16
+    Ordekk.setFlag(SpriteFlag.DestroyOnWall, true)
+})
+game.onUpdateInterval(500, function () {
+    araba = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 2 2 2 2 2 2 2 2 . . 
+        . . . . . 2 c 2 2 2 2 2 2 4 2 . 
+        . . . . 2 c c 2 2 2 2 2 2 4 c 2 
+        . . d 2 4 c c 2 4 4 4 4 4 4 c c 
+        . d 2 2 4 c b e e e e e e e 2 c 
+        . 2 2 2 4 b e e b b b e b b e 2 
+        . 2 2 2 2 2 e b b b b e b b b e 
+        . 2 2 2 2 e 2 2 2 2 2 e 2 2 2 e 
+        . 2 d d 2 e f e e e f e e e e e 
+        . d d 2 e e e f e e f e e e e e 
+        . e e e e e e e f f f e e e e e 
+        . e e e e f f f e e e e f f f f 
+        . . . e f f f f f e e f f f f f 
+        . . . . f f f f . . . . f f f . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    araba.setVelocity(-50, 0)
+    tiles.placeOnRandomTile(araba, sprites.vehicle.roadHorizontal)
+    araba.x = 200
+    araba.setFlag(SpriteFlag.DestroyOnWall, true)
+})
